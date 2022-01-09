40ecf83e1654d598f284aee0b150c7a8ff612425
omerfarukozbek <omerfarukozbek89@gmail.com>
2022-01-10 00:21:51 +0300

    asd

--- src/kdvhesap.java
+++ src/kdvhesap.java
@@ -3,12 +3,22 @@ import java.util.Scanner;
 public class kdvhesap {
     public static void main(String[] args) {
         Scanner input=new Scanner(System.in);
-        System.out.println("bir tutar giriniz=");
+        System.out.print("bir tutar giriniz=");
         float tutar=input.nextInt();
-        float kdvlitutar=tutar*1.18f;
-        float kdv=tutar*0.18f;
-        System.out.println("KDV'siz tutar="+tutar);
-        System.out.println("KDV'li tutar ="+kdvlitutar);
-        System.out.println("KDV ="+kdv);
+        if (tutar<=1000) {
+            float kdvlitutar = tutar * 1.18f;
+            float kdv = tutar * 0.18f;
+            System.out.println("KDV'siz tutar=" + tutar);
+            System.out.println("KDV'li tutar =" + kdvlitutar);
+            System.out.println("KDV =" + kdv);
+        }else {float kdvlitutar = tutar * 1.08f;
+            float kdv = tutar * 0.08f;
+            System.out.println("KDV'siz tutar=" + tutar);
+            System.out.println("KDV'li tutar =" + kdvlitutar);
+            System.out.println("KDV =" + kdv);
+
+
+        }
 
     }
+}
